978e14e8e99047bc0f083c72f96f86265d8c4cf1
MrFlexi <szaroletta@hotmail.com>
2019-04-29 16:48:36 +0200

    Servo Included

--- SolarServer1.0/SolarServer1.0.ino
+++ SolarServer1.0/SolarServer1.0.ino
@@ -8,10 +8,19 @@
 #define ANALOG_PIN_0 36
 int analog_value = 0;
 
+#include <ESP32Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+int servoPin = 18;
+
 void setup()
 {
   Serial.begin(115200);
-  delay(1000); // give me time to bring up serial monitor
+  myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 1000, 2000); 
+  delay(1000); // give me times to bring up serial monitor
   Serial.println("ESP32 Analog IN Test");
 }
 
@@ -20,4 +29,15 @@ void loop()
   analog_value = analogRead(ANALOG_PIN_0);
   Serial.println(analog_value);
   delay(500);
+
+for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+		// in steps of 1 degree
+		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+	}
+	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+	}
+
 }
