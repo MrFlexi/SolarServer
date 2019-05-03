4bacb19e2653ac8df3d28b500fed444dd3087098
MrFlexi <Szaroletta@hotmail.com>
2019-05-03 09:50:52 +0200

    ADC2

--- SolarServer1.0/SolarServer1.0.ino
+++ SolarServer1.0/SolarServer1.0.ino
@@ -1,11 +1,9 @@
 /****************************************************** 
-* ESP32 Analog Input Test 
-* Analog Input: ADC_1_0 pin ==> GPIO36 (VP).
+* Solar Server 1.0 
+* Analog Input: 
 * 
 * MJRoBot.org 6Sept17
 *****************************************************/
-//Analog Input
-#define ADC1_CHANNEL_6 35 // GPIO 35
 int analog_value = 0;
 
 #include <ESP32Servo.h>
@@ -13,19 +11,34 @@ int analog_value = 0;
 #include <U8g2lib.h>
 #include <Wire.h>
 
+#include <ESP32analogReadNonBlocking.h>
+
+
+		ESP32analogReadNonBlocking ADC_pin25(25, 100000);
+    ESP32analogReadNonBlocking ADC_pin26(26, 100000);
+    
+
+
+	uint8_t ArbitrationToken1; //Use one Aribtration Token for anything on ADC1, GPIO: 34, 35, 36, 37, 38, 39ADC_
+	uint8_t ArbitrationToken2; //Use one Arbitration Token for anything on ADC2, GPIO: 4, 12, 13, 14, 15, 25, 26, 27
+
+	uint32_t loopcounter;//loop counter for example to show that the code is non-blocking
+	uint32_t loopcounterSerialPrintTimer;
+
 
 
 Servo myservo;  // create servo object to control a servo
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
 
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // ESP32 Thing, HW I2C with pin remapping
 
 int pos = 0;    // variable to store the servo position
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
 int servoPin = 18;
 
 // assume 4x6 font, define width and height
-#define U8LOG_WIDTH 32
-#define U8LOG_HEIGHT 2
+#define U8LOG_WIDTH 64
+#define U8LOG_HEIGHT 4
 
 // allocate memory
 uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
@@ -41,11 +54,14 @@ double Amps = 0;
 
 void setup_display(void) {
   u8g2.begin();    
-  u8g2.setFont( u8g2_font_t0_11_mf );  // set the font for the terminal window
   u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer); // connect to u8g2, assign buffer
   u8g2log.setLineHeightOffset(0); // set extra space between lines in pixel, this can be negative
   u8g2log.setRedrawMode(0);   // 0: Update screen with newline, 1: Update screen for every char 
-  u8g2log.print("Mqtt Client...");
+  u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+  u8g2.drawGlyph(1, 20, 69);	
+  delay(2000);
+  u8g2.setFont( u8g2_font_6x12_tr);  // set the font for the terminal window
+  u8g2log.print("Solar Server...");
   u8g2log.print("\n");
 }
 
@@ -58,8 +74,7 @@ void setup()
   myservo.setPeriodHertz(50);    // standard 50 hz servo
 	myservo.attach(servoPin, 1000, 2000); 
   delay(1000); // give me times to bring up serial monitor
-  Serial.println("ESP32 Analog IN Test");
-
+  
   u8g2log.print("SolarServer");u8g2log.print("\n");
 
 
@@ -67,7 +82,7 @@ void setup()
 
 void Calcula_corrente()
 {
-  RawValue = analogRead(ADC1_CHANNEL_6);
+  //RawValue = analogRead(ADC1_CHANNEL_6);
   Voltage = RawValue * ( 3.3 / 4095.0);
   
   Amps = ((Voltage - ACSoffset) / mVperAmp);
@@ -77,19 +92,42 @@ void Calcula_corrente()
 
 }
 
+double GetVoltage(double reading){
+   // Reference voltage is 3v3 so maximum reading is 3v3 = 4095 in range 0 to 4095
+  if(reading < 1 || reading > 4095) return 0;
+  // return -0.000000000009824 * pow(reading,3) + 0.000000016557283 * pow(reading,2) + 0.000854596860691 * reading + 0.065440348345433;
+  return -0.000000000000016 * pow(reading,4) + 0.000000000118171 * pow(reading,3)- 0.000000301211691 * pow(reading,2)+ 0.001109019271794 * reading + 0.034143524634089;
+} 
+
+
 void loop()
 {
-  Calcula_corrente();
-  delay(200);
 
-//for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-//		// in steps of 1 degree
+ADC_pin25.tick(ArbitrationToken1);
+ADC_pin26.tick(ArbitrationToken2);
+
+
+
+	if (ADC_pin25.newValueFlag) { 
+	                          Serial.print("GPIO25 raw counts = "); 
+	                          Serial.print(ADC_pin25.counts); 
+                            Voltage = ADC_pin25.counts * ( 3.3 / 4095.0);
+                            Serial.print(" Voltage: "); 
+                            Serial.print(Voltage); 
+                            Serial.print(" Voltage new: "); 
+                            Voltage =  GetVoltage(ADC_pin25.counts);
+                            Serial.println(Voltage); 
+                            u8g2log.print("Raw ADC0:");u8g2log.print(ADC_pin25.counts);u8g2log.print("\n");
+                            u8g2log.print("Raw ADC1:");u8g2log.print(ADC_pin26.counts);u8g2log.print("\n");
+                            u8g2log.print("Voltage:");u8g2log.print(Voltage);u8g2log.print("\n");
+                            //Calcula_corrente();
+	                         }
+
+  
+
+
+
 //		myservo.write(pos);    // tell servo to go to position in variable 'pos'
-//		delay(15);             // waits 15ms for the servo to reach the position
-//	}
-//	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//		myservo.write(pos);    // tell servo to go to position in variable 'pos'
-//		delay(15);             // waits 15ms for the servo to reach the position
-//	}
+
 
 }
